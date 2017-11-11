aa20109e3f75c7af1210a30256b8a005f669caac
adriancarriger <acarriger@docutap.com>
2017-11-11 14:38:10 -0600

    calculate values dynamically

--- frosted-glass/glass-test-1/src/components/Blur.js
+++ frosted-glass/glass-test-1/src/components/Blur.js
@@ -1,9 +1,4 @@
 export default {
-  data () {
-    return {
-      ticking: false
-    }
-  },
   inserted (el, binding, vnode) {
     const position = window.getComputedStyle(el).position
     if (position !== 'fixed') { return }
@@ -15,34 +10,53 @@ export default {
 
     const blurContainer = document.createElement('div')
     const blurContent = document.createElement('div')
+    let topOffset = 0
 
-    blurContainer.style.position = 'fixed'
-    blurContainer.style.top = `${app.offsetTop}px`
-    blurContainer.style.left = '0'
-    blurContainer.style.right = '0'
-    blurContainer.style.height = '100px'
-    blurContainer.style.overflow = 'hidden'
-    blurContainer.style.transform = 'translate3d(0, 0, 0)'
+    // Styles
+    Object.assign(blurContainer.style, {
+      overflow: 'hidden',
+      transform: 'translate3d(0, 0, 0)'
+    })
 
-    blurContent.style.position = 'absolute'
-    blurContent.style.top = '0'
-    blurContent.style.left = '0'
-    blurContent.style.right = '0'
-    blurContent.style.filter = 'blur(5px)'
+    Object.assign(blurContent.style, {
+      position: 'absolute',
+      filter: 'blur(5px)'
+    })
 
     blurContent.appendChild(copy)
     blurContainer.appendChild(blurContent)
     document.querySelector('body').appendChild(blurContainer)
 
+    // Setup
+    window.addEventListener('scroll', onScroll)
+    window.addEventListener('resize', onResize)
+    onResize()
+
+    function onResize () {
+      const elementStyle = window.getComputedStyle(el)
+      const appStyle = window.getComputedStyle(app)
+      topOffset = parseInt(elementStyle.top, 10);
+
+      ['position', 'height', 'top', 'left', 'right'].forEach((item) => {
+        blurContainer.style[item] = elementStyle[item]
+      });
+
+      ['top', 'right', 'left'].forEach((item) => {
+        blurContent.style[item] = `-${elementStyle[item]}`
+      })
+
+      blurContent.style.width = appStyle.width
+      requestTick()
+    }
+
     function update () {
-      blurContent.style.top = `-${vnode.context.latestKnownScrollY}px`
+      const scrollOffset = vnode.context.latestKnownScrollY + topOffset
+      blurContent.style.top = `-${scrollOffset}px`
       vnode.context.ticking = false
     }
 
     function requestTick () {
-      if (!vnode.context.ticking) {
-        requestAnimationFrame(update)
-      }
+      if (!vnode.context.ticking) { requestAnimationFrame(update) }
       vnode.context.ticking = true
     }
 
@@ -50,7 +64,5 @@ export default {
       vnode.context.latestKnownScrollY = window.scrollY
       requestTick()
     }
-
-    window.addEventListener('scroll', onScroll)
   }
 }
