8144587426c75bfeba5c80e9661690b17f9b62e4
adriancarriger <acarriger@docutap.com>
2017-12-18 18:54:47 -0600

    refactor

--- image-navigation/test1/src/test.js
+++ image-navigation/test1/src/test.js
@@ -1,19 +1,14 @@
 // Example images were taken from http://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/template_matching/template_matching.html
 
-// Using => {"opencv": "^6.0.0"}
+// Using => {"opencv": "^5.0.0"}
 
 const cv = require('opencv');
 
-myreadImage('./Template_Matching_Original_Image.jpg').then((image) => {
-  const output = image.matchTemplate('./Template_Matching_Template_Image.jpg', 3);
-  const test = output[0].minMaxLoc();
+myreadImage('./images/original.jpg').then((image) => {
+  const output = image.matchTemplate('./images/template8-2.jpg', 3);
+  const matches = output.templateMatches(0.80, 1.0, 1, false);
 
-  printMethods(test);
-
-  // printMethods(output[0]);
-
-  // var matches = output[0].templateMatches(0.80, 1.0, 5, false);
-  // var matches = output[0].templateMatches(0.80, 1.0, 5, true, 0, 0);
+  console.log(matches[0]);
 });
 
 async function myreadImage(path) {
@@ -23,24 +18,4 @@ async function myreadImage(path) {
       resolve(image);
     });
   });
-}
-
-function printMethods(obj) {
-  const methods = getMethods(obj);
-  console.log(methods);
-  console.log(obj);
-}
-
-function getMethods(obj) {
-  var result = [];
-  for (var id in obj) {
-    try {
-      if (typeof(obj[id]) == "function") {
-        result.push(id + ": " + obj[id].toString());
-      }
-    } catch (err) {
-      result.push(id + ": inaccessible");
-    }
-  }
-  return result;
 }
