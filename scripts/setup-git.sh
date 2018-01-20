0ee435601eb0c2b6ba455913ac07f053a432c03c
adriancarriger <acarriger@docutap.com>
2018-01-20 07:34:51 -0600

    ci(lint): add commitlint

--- scripts/setup-git.sh
+++ scripts/setup-git.sh
@@ -1,3 +1,4 @@
 git config --global user.email "hello@adriancarriger.com"
 git config --global user.name "Adrian Carriger"
 git config --global push.default simple
+git push --set-upstream origin master
