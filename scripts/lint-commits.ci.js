88251f30078c40f1fb8ff6871488f055fccf3c83
adriancarriger <acarriger@docutap.com>
2018-01-25 18:38:11 -0600

    feat(app): add local commit linting

--- scripts/lint-commits.ci.js
+++ scripts/lint-commits.ci.js
@@ -2,20 +2,20 @@ const getConfig = require('semantic-release/lib/get-config');
 const getCommits = require('semantic-release/lib/get-commits');
 const logger = require('semantic-release/lib/logger');
 
-const verifyCommits = require('./verify-commits-plugin');
+const verifyCommits = require('./lint-commits.plugin');
 
 async function runCommitLint(opts) {
   const config = await getConfig(opts, logger);
-  const {plugins, options} = config;
+  const { plugins, options } = config;
 
-  await plugins.verifyConditions({options, logger});
-  const {commits, lastRelease} = await getCommits(
-    await plugins.getLastRelease({options, logger}),
+  await plugins.verifyConditions({ options, logger });
+  const { commits, lastRelease } = await getCommits(
+    await plugins.getLastRelease({ options, logger }),
     options.branch,
     logger
   );
 
-  await verifyCommits({}, {commits});
+  await verifyCommits({}, { commits });
 };
 
 runCommitLint().catch(error => {
