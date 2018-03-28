a1c6d0eb14fe79bc9c70da3bd38857866c9c4dee
Adrian Carriger <hello@adriancarriger.com>
2018-03-27 22:23:59 -0500

    add binary search

---
+++ algorithms/1/src/binary-search/binary-search.js
@@ -0,0 +1,21 @@
+function binarySearch(needle, haystack) {
+  let index, guess, iterations = 0, min = 0, max = haystack.length - 1;
+
+  while (index === undefined) {
+    iterations++;
+    guess = Math.floor( (min + max) / 2 );
+    if (haystack[guess] === needle) {
+      index = guess;
+    } else if (needle > haystack[guess]) {
+      min = guess + 1;
+    } else {
+      max = guess - 1;
+    }
+  }
+
+  return { index, iterations };
+}
+
+module.exports = {
+  binarySearch
+}
---
+++ algorithms/1/src/binary-search/binary-search.spec.js
@@ -0,0 +1,22 @@
+const { binarySearch } = require('./binary-search');
+
+const haystack = [
+  2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+  41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+];
+
+[
+  {needle: 53, expectedIndex: 15, expectedIterations: 3},
+  {needle: 97, expectedIndex: 24, expectedIterations: 5},
+  {needle: 2, expectedIndex: 0, expectedIterations: 4},
+  {needle: 31, expectedIndex: 10, expectedIterations: 4},
+  {needle: 5, expectedIndex: 2, expectedIterations: 3}
+]
+  .forEach(({ needle, expectedIndex, expectedIterations }) => {
+    it('should implement binary search',  () => {
+      const { index, iterations } = binarySearch(needle, haystack);
+
+      expect(index).toBe(expectedIndex);
+      expect(iterations).toBe(expectedIterations);
+    });
+  });
