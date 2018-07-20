51e0ca9df6b0fe3975c942c8e596cf28c6634cab
Adrian Carriger <hello@adriancarriger.com>
2018-07-19 20:01:51 -0500

    add wrapper

---
+++ vue/hello-world/src/components/MyButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <md-button v-bind="$attrs" v-on="$listeners">asdf</md-button>
+</template>
+
+<script>
+
+export default {
+  name: 'MyButton'
+}
+</script>
+
+<style scoped>
+/deep/ .md-button-content {
+  background: yellow;
+}
+</style>
--- vue/hello-world/src/main.js
+++ vue/hello-world/src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {
+  MdButton,
+  MdContent,
+  MdTabs
+} from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
 
 Vue.config.productionTip = false
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
 
 new Vue({
   render: h => h(App)
