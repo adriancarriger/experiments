c4ef8005e4062e66748aa7d0c9086f8f79e45db8
Adrian Carriger <hello@adriancarriger.com>
2018-11-21 07:43:19 -0600

    improve slack scraper

--- puppeteer/test1/src/temp.ts
+++ puppeteer/test1/src/temp.ts
@@ -1,25 +1,42 @@
-// in:#country-corners from:@John Lubke
+// search for something in Slack
 const messages = [];
+let waitTime = 1500;
+let longWait = 4000;
 
-setInterval(() => {
-  printItems();
-}, 1000);
-
-function printItems() {
+function saveItems() {
   const list = document.querySelector('.c-search_focus_managed_list');
   const next = document.querySelector('.c-search__pager__button_forward');
 
-  if (!list) {
-    console.log('DONE!');
+  if (list.length === 0) {
+    console.log('Could not find items! 😢');
+    setTimeout(() => {
+      saveItems();
+    }, longWait);
+
     return;
   }
 
-  for (var item of list.childNodes) {
+  for (const item of list.childNodes) {
+    const sender = item.querySelector('.c-message__sender_link');
     const textNode = item.querySelector('.c-search_message__body span');
     if (textNode) {
-      messages.unshift(textNode.innerText);
+      messages.push({ user: sender.innerText, text: textNode.innerText });
     }
   }
 
-  next.click();
+  if (next) {
+    next.click();
+
+    setTimeout(() => {
+      saveItems();
+    }, waitTime);
+  } else {
+    console.log('\n\n\n\n\n\nDONE!');
+    console.log('🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉');
+    console.log('\n\n');
+    messages.forEach(message => {
+      console.log(`\n${message.user}:`);
+      console.log(message.text);
+    });
+  }
 }
