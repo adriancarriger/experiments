eaad3c35cd00666b65eea4f29e7a171119005139
Adrian Carriger <hello@adriancarriger.com>
2019-01-28 21:07:31 -0600

    print out bitsets

---
+++ udacity/self-driving-intro/4-c++/5/main.cpp
@@ -0,0 +1,11 @@
+#include <bitset>
+#include <cstring>
+#include <iostream>
+
+int main() {
+  std::cout << "\n char A: \n" << std::bitset<8>('a') << "\n";
+  std::cout << "\n char B: \n" << std::bitset<8>('b') << "\n";
+  std::cout << "\n char C: \n" << std::bitset<8>('c') << "\n";
+
+  return 0;
+}
