9c75830ac60fd780b6d1948d6b0d679c7c30722e
Adrian Carriger <hello@adriancarriger.com>
2019-02-08 19:03:17 -0600

    add top k frequent

---
+++ algorithms/2/src/leetcode/leetcode/top-k-frequent-elements/top-k-frequent-element.spec.ts
@@ -0,0 +1,7 @@
+import topKFrequent from './top-k-frequent-element';
+
+describe('...', () => {
+  it('...', () => {
+    expect(topKFrequent([1, 1, 1, 2, 2, 3], 2)).toEqual([1, 2]);
+  });
+});
---
+++ algorithms/2/src/leetcode/leetcode/top-k-frequent-elements/top-k-frequent-element.ts
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+export default (nums, k) => {
+  const frequency = {};
+  for (let i = 0; i < nums.length; i++) {
+    frequency[nums[i]] = (frequency[nums[i]] || 0) + 1;
+  }
+
+  return Object.keys(frequency)
+    .sort((a, b) => frequency[b] - frequency[a])
+    .slice(0, k);
+};
