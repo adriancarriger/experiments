f246738f49c76d3c0162afc08277b64c373dd8f9
Adrian Carriger <hello@adriancarriger.com>
2019-03-28 12:09:34 -0500

    quick example

---
+++ mysql/1/README.md
@@ -0,0 +1,9 @@
+# DB Project
+
+## Comands
+
+- `docker-compose up -d`
+- `docker-compose exec db /bin/bash`
+- `mysql -uroot -pexample`
+- `USE mytest;`
+- `SHOW TABLES;`
---
+++ mysql/1/db/data/setup-schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE mytest;
+USE mytest;
+CREATE TABLE MyGuests (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP
+)
