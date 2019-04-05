c23cd876f928b2c9e53d0e5fdd45b4fc99a2a049
Adrian Carriger <hello@adriancarriger.com>
2019-04-05 15:55:33 -0500

    add form upload

--- file-upload/src/index.ts
+++ file-upload/src/index.ts
@@ -1,10 +1,13 @@
 import * as express from 'express';
 import * as fs from 'fs';
+import * as bodyParser from 'body-parser';
 
 const port = 8055;
 
 express()
+  .use(bodyParser.json())
   .post('/upload', (req: any, res) => {
+    console.log(req.body);
     req.pipe(fs.createWriteStream('uploads/my-test.png'));
     req.on('end', () => {
       res.json({ status: 'complete' });
