29aa6c5aac7e2dcbc3178bd4bc62f2445ec856c2
Adrian Carriger <hello@adriancarriger.com>
2019-05-31 19:39:14 -0500

    add more test cases

--- algorithms/2/src/leetcode/longest-palindromic-substring/longest.spec.ts
+++ algorithms/2/src/leetcode/longest-palindromic-substring/longest.spec.ts
@@ -1,39 +1,69 @@
-import longestPalindrome from './longest';
+import longestPalindrome, { longestFromIndex } from './longest';
 
 describe('...', () => {
-  it('...', () => {
-    expect(longestPalindrome('babad')).toBe('bab');
+  describe('longestPalindrome', () => {
+    it('...', () => {
+      expect(longestPalindrome('babad')).toBe('bab');
+    });
+
+    it('...', () => {
+      expect(longestPalindrome('cbbd')).toBe('bb');
+    });
+
+    it('...', () => {
+      expect(longestPalindrome('cbbbbd')).toBe('bbbb');
+    });
+
+    it('...', () => {
+      expect(longestPalindrome('bracecarz')).toBe('racecar');
+    });
+
+    it('...', () => {
+      expect(longestPalindrome('a')).toBe('a');
+    });
+
+    it('...', () => {
+      expect(longestPalindrome('')).toBe('');
+    });
+
+    it('...', () => {
+      expect(longestPalindrome('bb')).toBe('bb');
+    });
+
+    it('...', () => {
+      expect(longestPalindrome('ccc')).toBe('ccc');
+    });
+
+    it('...', () => {
+      expect(longestPalindrome('aaabaaaa')).toBe('aaabaaa');
+    });
+
+    it('...', () => {
+      expect(longestPalindrome('tattarrattat')).toBe('tattarrattat');
+    });
+
+    it('...', () => {
+      expect(longestPalindrome('abb')).toBe('bb');
+    });
+
+    it('...', () => {
+      const longInput =
+        'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb';
+      expect(longestPalindrome(longInput)).toBe(longInput);
+    });
   });
 
-  it('...', () => {
-    expect(longestPalindrome('cbbd')).toBe('bb');
-  });
+  describe('longestFromIndex', () => {
+    it('', () => {
+      expect(longestFromIndex('aba', 1)).toBe('aba');
+    });
 
-  it('...', () => {
-    expect(longestPalindrome('cbbbbd')).toBe('bbbb');
-  });
+    it('', () => {
+      expect(longestFromIndex('tattarrattat', 6)).toBe('tattarrattat');
+    });
 
-  it('...', () => {
-    expect(longestPalindrome('bracecarz')).toBe('racecar');
-  });
-
-  it('...', () => {
-    expect(longestPalindrome('a')).toBe('a');
-  });
-
-  it('...', () => {
-    expect(longestPalindrome('')).toBe('');
-  });
-
-  it('...', () => {
-    expect(longestPalindrome('bb')).toBe('bb');
-  });
-
-  it('...', () => {
-    expect(longestPalindrome('ccc')).toBe('ccc');
-  });
-
-  it('...', () => {
-    expect(longestPalindrome('aaabaaaa')).toBe('aaabaaa');
+    it('...', () => {
+      expect(longestFromIndex('abb', 2)).toBe('bb');
+    });
   });
 });
--- algorithms/2/src/leetcode/longest-palindromic-substring/longest.ts
+++ algorithms/2/src/leetcode/longest-palindromic-substring/longest.ts
@@ -1,6 +1,6 @@
 export default (input: string) => {
   let longest = input[0] || '';
-  for (let i = 0; i < input.length - 1; i++) {
+  for (let i = 0; i < input.length; i++) {
     const longestAtIndex = longestFromIndex(input, i);
     if (longestAtIndex.length > longest.length) {
       longest = longestAtIndex;
@@ -10,34 +10,50 @@ export default (input: string) => {
   return longest;
 };
 
-function longestFromIndex(input, index) {
-  let max = 0;
-  let min = 0;
-  for (let j = 0; j < input.length - index; j++) {
-    if (input[index + j] === input[index - j]) {
-      max = j;
-      min = j;
-    } else {
+export function longestFromIndex(input, index) {
+  let leftPointer = index;
+  let rightPointer = index;
+  let longest = input[index];
+  let temp = input[index];
+
+  while (true) {
+    let foundPalindrome = false;
+    if (leftPointer > 0) {
+      --leftPointer;
+      temp = input[leftPointer] + temp;
+      if (isPalindrome(temp)) {
+        longest = temp;
+        foundPalindrome = true;
+      }
+    }
+
+    if (rightPointer < input.length - 1) {
+      ++rightPointer;
+      temp = temp + input[rightPointer];
+      if (isPalindrome(temp)) {
+        longest = temp;
+        foundPalindrome = true;
+      }
+    }
+
+    if (!foundPalindrome) {
       break;
     }
   }
 
-  if (monoLetterWord(input.slice(index - min, index + max + 1))) {
-    max = max + (input[index] === input[index + max + 1] ? 1 : 0);
-    min = min + (input[index] === input[index - min - 1] ? 1 : 0);
-  }
-
-  return input.slice(index - min, index + max + 1);
+  return longest;
 }
 
-function monoLetterWord(input) {
-  let lastLetter = input[0];
-  for (let i = 1; i < input.length; i++) {
-    if (input[i] !== lastLetter) {
+function isPalindrome(input) {
+  let leftPointer = 0;
+  let rightPointer = input.length - 1;
+
+  while (leftPointer <= rightPointer) {
+    if (input[leftPointer] !== input[rightPointer]) {
       return false;
     }
-
-    lastLetter = input[i];
+    leftPointer++;
+    rightPointer--;
   }
 
   return true;
