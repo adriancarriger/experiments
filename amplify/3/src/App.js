b95f5fb3c5f70bbdea1420b607fd8c9bff4198c0
Adrian Carriger <hello@adriancarriger.com>
2019-08-11 14:29:43 -0500

    add graphql

--- amplify/3/src/App.js
+++ amplify/3/src/App.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Auth, Hub } from 'aws-amplify';
+
+function App(props) {
+  useEffect(() => {
+    Hub.listen('auth', data => {
+      const { payload } = data;
+      console.log('A new auth event has happened: ', data);
+      if (payload.event === 'signIn') {
+        console.log('a user has signed in!');
+      }
+      if (payload.event === 'signOut') {
+        console.log('a user has signed out!');
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world!</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+        <button onClick={checkUser}>Check User</button>
+        <button onClick={signOut}>Sign Out</button>
+        <button onClick={() => Auth.federatedSignIn({ provider: 'Google' })}>
+          Sign In with Google
+        </button>
       </header>
     </div>
   );
+}
+
+function checkUser() {
+  Auth.currentAuthenticatedUser()
+    .then(user => console.log({ user }))
+    .catch(err => console.log(err));
+}
+
+function signOut() {
+  Auth.signOut()
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 }
 
 export default App;
