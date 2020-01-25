0baf73dd3cf7e6ba91b16c683c33728180bb76e7
Adrian Carriger <hello@adriancarriger.com>
2020-01-25 11:22:37 -0600

    base eval

--- webpack/1/webpack.config.js
+++ webpack/1/webpack.config.js
@@ -4,37 +4,19 @@ module.exports = {
   target: 'node',
 
   entry: {
-    app: './src/index.js',
-    markdown: './src/hi.md'
+    app: './src/index.js'
   },
-
-  // output: {
-  //   filename: '[name].js'
-  // },
 
   mode: 'production',
   optimization: {
-    minimize: false,
-    splitChunks: {
-      // include all types of chunks
-      chunks: 'all'
-    }
+    minimize: false
   },
 
   module: {
     rules: [
       {
         test: /\.(png|jpe?g|gif|js)$/i,
-        use: [
-          {
-            // loader: 'file-loader'
-            loader: path.resolve('lib/my-loader.js')
-          }
-        ]
-      },
-      {
-        test: /\.md$/,
-        use: [{ loader: path.resolve('lib/markdown-loader.js') }, { loader: 'raw-loader' }]
+        use: [{ loader: path.resolve('lib/my-loader.js') }]
       }
     ]
   }
